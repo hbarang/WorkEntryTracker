fb04a06f6c207bb8cf2ba28413f33a6654f0d170
hbarang <hbarangenc@gmail.com>
2019-09-17 21:04:26 +0300

    Updates

---
+++ precache-manifest.d07debe4104ec95ec8bcffb0e370ba39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4c5f472f3221bc5bb88d468d3da7f0",
+    "url": "/WorkEntryTracker/index.html"
+  },
+  {
+    "revision": "d26f7303fa58caa47a70",
+    "url": "/WorkEntryTracker/static/css/main.0f3970c1.chunk.css"
+  },
+  {
+    "revision": "cb1489bc4b730445dd31",
+    "url": "/WorkEntryTracker/static/js/2.93aa4487.chunk.js"
+  },
+  {
+    "revision": "d26f7303fa58caa47a70",
+    "url": "/WorkEntryTracker/static/js/main.cc320f00.chunk.js"
+  },
+  {
+    "revision": "e19be5ae6f0bfe92a1d9",
+    "url": "/WorkEntryTracker/static/js/runtime~main.fde0538a.js"
+  }
+]);
